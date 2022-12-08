92b8bcda60d38a47579c265af84f2c2efd677231
triveni01 <trivenisarode01@gmail.com>
2022-12-08 14:42:54 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,8 @@
+num1=25
+num2=4
+
+print(f"Addition {num1} + {num2} = {num1+num2}")
+print(f"Subtraction {num1} - {num2} = {num1-num2}")
+print(f"Multiplication {num1} * {num2} = {num1*num2}")
+print(f"Division {num1} / {num2} = {num1/num2}")
+
